f5aa704be19c760a45e35d9e08b5c8c3ce0bef3e
Dmitrii <kubatads@gmail.com>
2021-10-17 19:17:48 +0300

    dorabotka dz module4

--- js/main.js
+++ js/main.js
@@ -27,63 +27,12 @@ checkCommentLength('check length message - false', 10);
 
 // Генерация массива с описанием фотографии от пользователя
 
-const ID_PHOTO = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-  21,
-  22,
-  23,
-  24,
-  25,
+const DESCRIPTION_PHOTO = [
+  'Фотография из последней поездки',
+  'Мы в путешествии',
+  'Фото на даче',
+  'Жить в кайф',
 ];
-
-const URL_PHOTO = [
-  'photos/1.jpg',
-  'photos/2.jpg',
-  'photos/3.jpg',
-  'photos/4.jpg',
-  'photos/5.jpg',
-  'photos/6.jpg',
-  'photos/7.jpg',
-  'photos/8.jpg',
-  'photos/9.jpg',
-  'photos/10.jpg',
-  'photos/11.jpg',
-  'photos/12.jpg',
-  'photos/13.jpg',
-  'photos/14.jpg',
-  'photos/15.jpg',
-  'photos/16.jpg',
-  'photos/17.jpg',
-  'photos/18.jpg',
-  'photos/19.jpg',
-  'photos/20.jpg',
-  'photos/21.jpg',
-  'photos/22.jpg',
-  'photos/23.jpg',
-  'photos/24.jpg',
-  'photos/25.jpg',
-];
-
-const DESCRIPTION_PHOTO = 'Фотография из последней поездки';
 
 const COMMENTS_LIST = [
   'Всё отлично!',
@@ -93,10 +42,6 @@ const COMMENTS_LIST = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-
-function createMessage() {
-  return COMMENTS_LIST[getRandomInt(0,5)];
-}
 
 const NAME_LIST = [
   'Иван Степанов',
@@ -109,50 +54,27 @@ const NAME_LIST = [
 ];
 
 const PHOTO_INFO_COUNT = 25;
-
-
-//shuffle функция для смешевания порядка элементов в массиве
-
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-  return array;
-}
-
-//функция для смешивания порядка элементов в массиве + отдавать по одному элементу из массива
-function shuffledShift(shuffleShiftKey) {
-  shuffle(shuffleShiftKey);
-  return shuffleShiftKey.shift();
-}
+let generatedIdPhoto = 0;
+let generatedUrlPhoto = 0;
 
 const getPhotoInfo = () => {
-  const LIKES_PHOTO = getRandomInt(1,300);
   const COMMENT = {
     id: getRandomInt(1000,9000),
-    avatar: 'img/avatar-' + getRandomInt(1,6) + '.svg',
-    message: createMessage(),
+    avatar: `img/avatar-${  getRandomInt(1,6)  }.svg`,
+    message: COMMENTS_LIST[getRandomInt(0,COMMENTS_LIST.length - 1)],
     name: NAME_LIST[getRandomInt(0,6)],
   };
+  generatedIdPhoto++;
+  generatedUrlPhoto++;
   return {
-    id: shuffledShift(ID_PHOTO),
-    url: shuffledShift(URL_PHOTO),
-    description: DESCRIPTION_PHOTO,
-    likes: LIKES_PHOTO,
+    id: generatedIdPhoto,
+    url: `photos/ ${ generatedUrlPhoto }.jpg`,
+    description: DESCRIPTION_PHOTO[getRandomInt(0,DESCRIPTION_PHOTO.length - 1)],
+    likes: getRandomInt(1,300),
     comments: COMMENT,
   };
 };
 
+// временное отключение линтера, нужно для дз
+// eslint-disable-next-line no-unused-vars
 const photoInfo = Array.from({length: PHOTO_INFO_COUNT}, getPhotoInfo);
-
-photoInfo;
