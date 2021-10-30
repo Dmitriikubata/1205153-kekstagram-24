ca0d75ccab8e005ef5b4b96ec622a0d417df9acd
Dmitrii <kubatads@gmail.com>
2021-10-30 20:59:42 +0300

    module7task1 dorabotka

--- js/addPicture.js
+++ js/addPicture.js
@@ -1,20 +1,18 @@
 import {photoInfo} from './data.js';
 
-const photoData = photoInfo;
-
 const picContainer = document.querySelector('.pictures');
 const picTemplateFragment = document.querySelector('#picture').content;
 const picTemplate = picTemplateFragment.querySelector('a');
 const picFragment = document.createDocumentFragment();
 
-photoData.forEach((photo) =>
-{
+photoInfo.forEach( (photo) => {
   const picElement = picTemplate.cloneNode(true);
   picElement.querySelector('.picture__img').src = photo.url;
   picElement.querySelector('.picture__likes').textContent = photo.likes;
-  picElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  picElement.querySelector('.picture__comments').textContent = photo.comments ? photo.comments.length : 0;
+
   picFragment.appendChild(picElement);
-});
+} );
 
 picContainer.appendChild(picFragment);
 
